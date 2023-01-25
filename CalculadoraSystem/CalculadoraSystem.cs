4cf096d0776dfbdee5a1457d0020528de62ddcc5
Jacobo <vocosjacobo00@gmail.com>
2023-01-25 17:39:46 -0300

    Agrego Respositorio

---
+++ CalculadoraSystem/CalculadoraSystem.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemCalculadora;
+
+public class CalculadoraSystem
+{
+    public int Sumar(string nro)
+    {
+        if (string.IsNullOrEmpty(nro))
+            return 0;
+
+        var resultadoFinal = 0;
+        var valoresParaSumar = new List<int>();
+        var valores = nro.Split(",");
+
+        foreach (var valor in valores)
+        {
+
+            var valorEntero = int.Parse(valor);
+            valoresParaSumar.Add(valorEntero);
+
+        }
+
+        foreach (var i in valoresParaSumar)
+        {
+            resultadoFinal = resultadoFinal + i;
+        }
+
+        return resultadoFinal;
+
+    }
+
+    public int SumarConDistintosDelimitadores(string nro)
+    {
+        if (string.IsNullOrEmpty(nro))
+            return 0;
+        var resultadoFinal = 0;
+        var valoresParaSumar = new List<int>();
+        var valores = nro.Split(',',';','/');
+
+        foreach (var valor in valores)
+        {
+
+            var valorEntero = int.Parse(valor);
+            valoresParaSumar.Add(valorEntero);
+
+        }
+
+        foreach (var i in valoresParaSumar)
+        {
+            resultadoFinal = resultadoFinal + i;
+        }
+
+        return resultadoFinal;
+    }
+
+
+
+    //Punto 5)
+
+    public string ValidarNumerosNegativos(string nro)
+    {
+        var nroNegativo = int.Parse(nro);
+
+        if (nroNegativo < 0)
+            throw new Exception("el numero es negativo");
+
+        return nro;
+    }
+
+    //Punto 6
+
+    public string CaracterEnviadoComoNumeroSeparadoPorComa(string nro)
+    {
+        var caracterIngresado = int.Parse(nro);
+        var x = 2;
+
+        if(caracterIngresado != x)
+
+        throw new Exception("SOLO numeros");
+
+        return nro;
+
+    }
+
+    //metodo para numeros mayores a 100
+}
+
---
+++ TestCalculadoraSystem/CalculadoraTest.cs
@@ -0,0 +1,106 @@
+using SystemCalculadora;
+using Xunit;
+
+namespace TestCalculadoraSystem;
+
+public class CalculadoraTest
+{
+    [Fact]
+    public void Sumar_EnvioVacio_ReciboCero()
+    {
+        // arrange
+
+        var calculadorda = new CalculadoraSystem();
+
+
+        // act 
+
+        var resultado = calculadorda.Sumar("");
+
+        // assert
+        Assert.Equal(0, resultado);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("3,4", 7)]
+    [InlineData("5,6", 11)]
+
+    public void Suma_NumerosSeparadosPorComa_DevuelveSumaDeLosNumeros(string valores, int sumaValores)
+    {
+        // arrange
+
+        var calculadorda = new CalculadoraSystem();
+
+
+        // act 
+
+        var resultado = calculadorda.Sumar(valores);
+
+        // assert
+        Assert.Equal(sumaValores, resultado);
+    }
+    [Theory]
+    [InlineData("1;2", 3)]
+    [InlineData("3;4", 7)]
+    [InlineData("5/6", 11)]
+
+    public void Suma_NumerosSeparadosPorCualquierDelimitador_DevuelveSumaDeLosNumeros(string valores, int sumaValores)
+    {
+        // arrange
+
+        var calculadorda = new CalculadoraSystem();
+
+
+        // act 
+
+        var resultado = calculadorda.SumarConDistintosDelimitadores(valores);
+
+        // assert
+        Assert.Equal(sumaValores, resultado);
+    }
+
+    [Theory]
+    [InlineData("-1")]
+    [InlineData("-2")]
+    [InlineData("-3")]
+
+    public void nroNegativos_DevuelveExeption(string nroNegativo)
+    {
+        // arrange
+
+        var calculadorda = new CalculadoraSystem();
+
+
+        // act 
+
+       // var numeroNegativoIngresado = calculadorda.ValidarNumerosNegativos(nroNegativo);
+
+        // assert
+        Assert.Throws<Exception>(() => calculadorda.ValidarNumerosNegativos(nroNegativo));
+
+    }
+    [Theory]
+    [InlineData("a,b")]
+    [InlineData("c,d")]
+    [InlineData("e,f")]
+
+    public void ExeptionCaracterIngresadoComoNumero(string caracterIngresado)
+    {
+        // arrange
+
+        var calculadorda = new CalculadoraSystem();
+
+
+        // act 
+
+       // var resultado = calculadorda.CaracterEnviadoComoNumeroSeparadoPorComa(nro);
+
+        // assert
+        Assert.Throws<Exception>(() => calculadorda.CaracterEnviadoComoNumeroSeparadoPorComa(caracterIngresado));
+    }
+
+    // theory para numeros mayores a 100
+
+}
+
